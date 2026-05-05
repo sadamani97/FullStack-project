e17df42cb0559c1f119c734fe2d3e1b15233d5da
sadamani97 <sadamanism97@gmail.com>
2026-05-05 23:13:07 +0530

    integration error fix

--- backend/server.ts
+++ backend/server.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost", "http://localhost:5173"],
     credentials: true,
   })
 );
